2d064c63b96b443055b40db153721a1cb149f07f
VisinSky <visin@live.cn>
2026-01-11 14:25:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,4 +21,6 @@ echo 'src-git ddnsgo  https://github.com/sirpdboy/luci-app-ddns-go' >>feeds.conf
 echo 'src-git watchdog https://github.com/sirpdboy/luci-app-watchdog' >>feeds.conf.default
 echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >>feeds.conf.default
 git clone https://github.com/DustReliant/luci-app-filetransfer.git package/luci-app-filetransfer
+git clone https://github.com/timsaya/openwrt-bandix.git package/openwrt-bandix
+git clone https://github.com/timsaya/luci-app-bandix.git package/luci-app-bandix
 # echo 'src-git kwrtpackages https://github.com/kiddin9/kwrt-packages.git' >>feeds.conf.default
